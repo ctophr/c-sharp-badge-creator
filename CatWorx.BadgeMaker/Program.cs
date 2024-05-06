271ebc51b99b4f10c9d30cd7ac8088e43eb4c23b
ctophr <ctophr.dubois@gmail.com>
2024-05-06 15:23:07 -0700

    Get employee name prompt running in CLI

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace Catworx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter employee name, or enter blank to quit: ");
+                // Read a name from the console and assign it to a variable
+                // ?? is a coalescing operator that tells ReadLine how to handle a null input value,
+                // if null is found, it is replaced with ""
+                // necessary because our list can only have string values ... not nulls
+                // won't run without it apparently.
+                string input = Console.ReadLine() ?? "";
+
+                // Break if user enters an empty string
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+    }
+}
